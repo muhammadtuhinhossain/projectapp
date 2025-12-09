3a929f04358e77daef8ba0b5ae6ad6befe579839
MohammadTuhin1 <tuhinhossain55555@gamil.com>
2025-12-09 18:07:52 +0600

    Using set Type

--- bin/set.dart
+++ bin/set.dart
@@ -3,6 +3,14 @@ void main(){
   var fruits=<String>{};
   fruits.add('Apple');
   fruits.add('Banana');
-  fruits.add('Apple');
+  fruits.add('Apple');//Duplicate ignored
   print(fruits);
+
+  //methode 2: Using 'set<Type>'
+  Set<String> city={};
+  city.add('Dhaka');
+  city.add('Pabna');
+  city.add('Rajshahi');
+  city.add('Dhaka');
+  print(city);
 }
