4a64d4814cc68166e07c91d95b5b7397695ea868
MohammadTuhin1 <tuhinhossain55555@gamil.com>
2025-12-09 18:21:41 +0600

    Using Union Operation

---
+++ bin/Union.dart
@@ -0,0 +1,7 @@
+void main(){
+//Set Operation:Union
+//Combines all unique element for both set
+  var a={'Turkey','Bangladesh','Iran'};
+  var b={'Turkey','London','Mali'};
+  print(a.union(b));
+}
--- bin/set.dart
+++ bin/set.dart
@@ -14,10 +14,5 @@ void main(){
   city.add('Dhaka');
   print(city);
 
-  //Set Operation:Union
-  //Combines all unique element for both set
-  var a={'Turkey','Bangladesh','Iran'};
-  var b={'Turkey','London','Mali'};
-  print(a.union(b));
 }
 
