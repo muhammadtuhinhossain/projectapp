3fabdc4549db6ee28b34e0e9fcda6ddf1e3cc896
MohammadTuhin1 <tuhinhossain55555@gamil.com>
2025-12-09 19:50:53 +0600

    Intersection Operation

---
+++ bin/intersection.dart
@@ -0,0 +1,7 @@
+void main(){
+  //Set Operation:Intersection
+  //print the common value between the two sets
+  var a={'Cat','Dog','Rat','fish'};
+  var b={'Cat','Cow','Goat','fish'};
+  print(a.intersection(b));
+}
